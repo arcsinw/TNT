81a4ebb2d554e9d1e9d0770d5724b447e0520402
arcsinw <wangx86@live.com>
2018-06-08 22:07:03 +0800

    Initial commit

---
+++ TNT/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+namespace TNT
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void AskButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string text = await Utils.SpeechToTextAsync();
+                inputTextBox.Text = text;
+            }
+            catch (System.Runtime.InteropServices.COMException ex) when (ex.HResult == unchecked((int)0x80045509))
+            {
+                ContentDialog Dialog = new ContentDialog()
+                {
+                    Title = "The speech privacy policy was not accepted",
+                    Content = "You need to turn on a button called 'Get to know me'...",
+                    PrimaryButtonText = "Shut up",
+                    SecondaryButtonText = "Shut up and show me the setting"
+                };
+                if (await Dialog.ShowAsync() == ContentDialogResult.Secondary)
+                {
+                    const string uriToLaunch = "ms-settings:privacy-speechtyping";
+                    //"http://stackoverflow.com/questions/42391526/exception-the-speech-privacy-policy-" + 
+                    //"was-not-accepted-prior-to-attempting-a-spee/43083877#43083877";
+                    var uri = new Uri(uriToLaunch);
+
+                    var success = await Windows.System.Launcher.LaunchUriAsync(uri);
+
+                    if (!success) await new ContentDialog
+                    {
+                        Title = "Oops! Something went wrong...",
+                        Content = "The settings app could not be opened.",
+                        PrimaryButtonText = "Shut your mouth up!"
+                    }.ShowAsync();
+                }
+            }
+
+        }
+    }
+}
---
+++ TNT/Utils.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Media.SpeechRecognition;
+using Windows.Media.SpeechSynthesis;
+using Windows.Storage.Streams;
+
+namespace TNT
+{
+    public class Utils
+    {
+        private static SpeechRecognizer _recognizer = new SpeechRecognizer();
+        private static SpeechSynthesizer _synthesizer = new SpeechSynthesizer();
+
+        /// <summary>
+        /// 语音转文字
+        /// </summary>
+        /// <returns></returns>
+        public static async Task<string> SpeechToTextAsync()
+        {
+            var recognizedText = string.Empty;
+
+            //Max time to wait for the speaker to speak
+            _recognizer.Timeouts.InitialSilenceTimeout = TimeSpan.FromSeconds(3);
+
+            await _recognizer.CompileConstraintsAsync();
+
+            SpeechRecognitionResult result = await _recognizer.RecognizeWithUIAsync();
+            if (result.Status == SpeechRecognitionResultStatus.Success)
+            {
+                recognizedText = result.Text;
+            }
+            return recognizedText;
+        }
+
+        /// <summary>
+        /// 文字转语音
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static async Task<IRandomAccessStream> TextToSpeechAsync(string text)
+        {
+            IRandomAccessStream stream = await _synthesizer.SynthesizeTextToStreamAsync(text);
+            return stream;
+        }
+    }
+}
